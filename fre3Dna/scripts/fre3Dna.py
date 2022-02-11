be2317a5c132ba4433ac5c430da980b66c123df4
elija-feigl <elija.feigl@gmail.com>
2022-02-11 14:15:22 +0100

    dev (script) cleanup format command docstrings use click.Path (requires click>=8.0) use new version improved help and version keywords autoformat

--- fre3Dna/scripts/fre3Dna.py
+++ fre3Dna/scripts/fre3Dna.py
@@ -1,93 +1,86 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
-#
-# Copyright (C) 2021  Elija Feigl
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see https://www.gnu.org/licenses/gpl-3.0.html.
-
+# Copyright (C) 2021-Present  Elija Feigl
+# Full GPL-3 License can be found in `LICENSE` at the project root.
+""" free form DNA Origami
+"""
 import logging
 from pathlib import Path
 
 import click
 import pandas as pd
-
-from fre3Dna.core.attract_prep import get_nicks, get_scaffold_id
-from fre3Dna.network.graph import Graph
+from fre3Dna import get_version
+from fre3Dna.core.attract_prep import get_nicks
+from fre3Dna.core.attract_prep import get_scaffold_id
 from fre3Dna.data.structure import Structure
-from fre3Dna.version import get_version
+from fre3Dna.network.graph import Graph
 
-""" free form DNA Origami
-"""
+
 logger = logging.getLogger(__name__)
 
 
-def print_version(ctx, param, value):
+def print_version(ctx, _, value):
+    """click print version."""
     if not value or ctx.resilient_parsing:
         return
     click.echo(get_version())
     ctx.exit()
 
 
-@click.group()
-@click.option('--version', is_flag=True, callback=print_version,
-              expose_value=False, is_eager=True)
+@click.group(context_settings=dict(help_option_names=["-h", "--help"]))
+@click.option(
+    "-v",
+    "--version",
+    is_flag=True,
+    help="Show __version__ and exit.",
+    callback=print_version,
+    expose_value=False,
+    is_eager=True,
+)
 def cli():
     pass
 
 
 @cli.command()
-@click.argument('top', type=click.Path(exists=True))
-@click.argument('conf', type=click.Path(exists=True))
+@click.argument("top", type=click.Path(exists=True, resolve_path=True, path_type=Path))
+@click.argument("conf", type=click.Path(exists=True, resolve_path=True, path_type=Path))
 def prep(top, conf):
-    """ prep run with modified potential by creating required nick_list and scaffold number
-
-        TOP is the name of the design file [.top]\n
-        CONF is the scaffold strand sequence file [.oxdna, .dat]\n
+    """\b
+    prep run with modified potential by creating required nick_list and scaffold number
+    \b
+    TOP is the name of the design file [.top]
+    CONF is the scaffold strand sequence file [.oxdna, .dat]
     """
     df_top = pd.read_csv(
-        top, delim_whitespace=True,
-        skiprows=1, usecols=[0, 2, 3], names=["strand", "5p", "3p"]
+        top, delim_whitespace=True, skiprows=1, usecols=[0, 2, 3], names=["strand", "5p", "3p"]
     )
     df_conf = pd.read_csv(
-        conf, delim_whitespace=True,
-        skiprows=[0, 1, 2], usecols=[0, 1, 2], names=["x", "y", "z"]
+        conf, delim_whitespace=True, skiprows=[0, 1, 2], usecols=[0, 1, 2], names=["x", "y", "z"]
     )
     scaffold_id = get_scaffold_id(df_top)
-    logger.info(f"53_scaffold = {scaffold_id}")
+    logger.info("53_scaffold = %s", scaffold_id)
     print(f"53_nicks = {get_nicks(df_top, df_conf, scaffold_id)}")
 
 
 @cli.command()
-@click.argument('top', type=click.Path(exists=True))
-@click.argument('conf', type=click.Path(exists=True))
-@click.argument('forces', type=click.Path(exists=True))
-@click.argument('cutoff', type=float)
-@click.option('--o', 'out',  type=str, default=None,
-              help="new file name, default to conf-name_out")
+@click.argument("top", type=click.Path(exists=True, resolve_path=True, path_type=Path))
+@click.argument("conf", type=click.Path(exists=True, resolve_path=True, path_type=Path))
+@click.argument("forces", type=click.Path(exists=True, resolve_path=True, path_type=Path))
+@click.argument("cutoff", type=float)
+@click.option("--o", "out", type=str, default=None, help="new file name, default to conf-name_out")
 def pipeline(top, conf, forces, cutoff, out):
-    """ close staples perform analysis and print new files and sequences
-
-        TOP is the name of the design file [.top]\n
-        CONF is the scaffold strand sequence file [.oxdna, .dat]\n
-        FORCES is the oxDNA external force file containing the basepair information []\n
-        CUTOFF multiple of Backbone-Backbone distance to be considered for staple routing\n
+    """\b
+    close staples perform analysis and print new files and sequences
+    \b
+    TOP is the name of the design file [.top]
+    CONF is the scaffold strand sequence file [.oxdna, .dat]
+    FORCES is the oxDNA external force file containing the basepair information []
+    CUTOFF multiple of Backbone-Backbone distance to be considered for staple routing
     """
     if out is None:
-        out = f"{Path(conf).stem}_out"
+        out = f"{conf.stem}_out"
     struct = Structure()
-    struct.generate_from_oxDNA(top=Path(top), conf=Path(conf))
-    struct.assign_basepairs(forces=Path(forces))
+    struct.generate_from_oxDNA(top=top, conf=conf)
+    struct.assign_basepairs(forces=forces)
     struct.categorise_structure()
     weighted_edges = struct.generate_connectivity(cutoff=cutoff)
 
@@ -105,25 +98,25 @@ def pipeline(top, conf, forces, cutoff, out):
 
 
 @cli.command()
-@click.argument('top', type=click.Path(exists=True))
-@click.argument('conf', type=click.Path(exists=True))
-@click.argument('forces', type=click.Path(exists=True))
-@click.argument('cutoff', type=float)
-@click.option('--o', 'out',  type=str, default=None,
-              help="new file name, default to conf-name_out")
+@click.argument("top", type=click.Path(exists=True, resolve_path=True, path_type=Path))
+@click.argument("conf", type=click.Path(exists=True, resolve_path=True, path_type=Path))
+@click.argument("forces", type=click.Path(exists=True, resolve_path=True, path_type=Path))
+@click.argument("cutoff", type=float)
+@click.option("--o", "out", type=str, default=None, help="new file name, default to conf-name_out")
 def staple(top, conf, forces, cutoff, out):
-    """ close staples
-
-        TOP is the name of the design file [.top]\n
-        CONF is the scaffold strand sequence file [.oxdna, .dat]\n
-        FORCES is the oxDNA external force file containing the basepair information []\n
-        CUTOFF multiple of Backbone-Backbone distance to be considered for staple routing\n
+    """\b
+    close staples
+    \b
+    TOP is the name of the design file [.top]
+    CONF is the scaffold strand sequence file [.oxdna, .dat]
+    FORCES is the oxDNA external force file containing the basepair information []
+    CUTOFF multiple of Backbone-Backbone distance to be considered for staple routing
     """
     if out is None:
-        out = f"{Path(conf).stem}_out"
+        out = f"{conf.stem}_out"
     struct = Structure()
-    struct.generate_from_oxDNA(top=Path(top), conf=Path(conf))
-    struct.assign_basepairs(forces=Path(forces))
+    struct.generate_from_oxDNA(top=top, conf=conf)
+    struct.assign_basepairs(forces=forces)
     struct.categorise_structure()
     weighted_edges = struct.generate_connectivity(cutoff=cutoff)
 
@@ -136,21 +129,22 @@ def staple(top, conf, forces, cutoff, out):
 
 
 @cli.command()
-@click.argument('top', type=click.Path(exists=True))
-@click.argument('conf', type=click.Path(exists=True))
-@click.argument('forces', type=click.Path(exists=True))
-@click.argument('cutoff', type=float)
+@click.argument("top", type=click.Path(exists=True, resolve_path=True, path_type=Path))
+@click.argument("conf", type=click.Path(exists=True, resolve_path=True, path_type=Path))
+@click.argument("forces", type=click.Path(exists=True, resolve_path=True, path_type=Path))
+@click.argument("cutoff", type=float)
 def analyse(top, conf, forces, cutoff):
-    """ analyse structure graph
-
-        TOP is the name of the design file [.top]\n
-        CONF is the scaffold strand sequence file [.oxdna, .dat]\n
-        FORCES is the oxDNA external force file containing the basepair information []\n
-        CUTOFF multiple of Backbone-Backbone distance to be considered for staple routing\n
+    """\b
+    analyse structure graph
+    \b
+    TOP is the name of the design file [.top]
+    CONF is the scaffold strand sequence file [.oxdna, .dat]
+    FORCES is the oxDNA external force file containing the basepair information []
+    CUTOFF multiple of Backbone-Backbone distance to be considered for staple routing
     """
     struct = Structure()
-    struct.generate_from_oxDNA(top=Path(top), conf=Path(conf))
-    struct.assign_basepairs(forces=Path(forces))
+    struct.generate_from_oxDNA(top=top, conf=conf)
+    struct.assign_basepairs(forces=forces)
     struct.categorise_structure()
     struct.structure_stats()
 
@@ -161,18 +155,18 @@ def analyse(top, conf, forces, cutoff):
 
 
 @cli.command()
-@click.argument('top', type=click.Path(exists=True))
-@click.argument('conf', type=click.Path(exists=True))
-@click.option('--o', 'out',  type=str, default=None,
-              help="sequence file name, default to conf name")
+@click.argument("top", type=click.Path(exists=True, resolve_path=True, path_type=Path))
+@click.argument("conf", type=click.Path(exists=True, resolve_path=True, path_type=Path))
+@click.option("--o", "out", type=str, default=None, help="sequence file name, default to conf name")
 def sequence(top, conf, out):
-    """ analyse structure graph
-
-        TOP is the name of the design file [.top]\n
-        CONF is the scaffold strand sequence file [.oxdna, .dat]\n
+    """\b
+    analyse structure graph
+    \b
+    TOP is the name of the design file [.top]
+    CONF is the scaffold strand sequence file [.oxdna, .dat]
     """
     if out is None:
-        out = Path(conf).stem
+        out = conf.stem
     struct = Structure()
-    struct.generate_from_oxDNA(top=Path(top), conf=Path(conf))
+    struct.generate_from_oxDNA(top=top, conf=conf)
     struct.write_sequences(out)
